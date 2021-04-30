f01cf3f9dcb8b444c055e27698698f5bdacc5bb8
Jagruti <mohitejagruti5@gmail.com>
2021-04-30 14:39:56 -0500

    latitude is added in a code

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
+import SeasonsDisplay from "./SeasonsDisplay";
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {lat: null};
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                this.setState({lat: position.coords.latitude})
+            },
+            (error) => console.log(error)
+        );
+    }
     render() {
         return (
             <div>
-                Hello
+                 Latitude: {this.state.lat}
+            <SeasonsDisplay/>
             </div>
         );
     }
---
+++ src/Components/SeasonsDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SeasonsDisplay() {
+    return (
+        <div>
+            Seasons Display
+        </div>
+    )
+}
